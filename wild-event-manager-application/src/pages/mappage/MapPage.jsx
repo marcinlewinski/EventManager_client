34c1f0298d2374ed3c628f0166fc168e650e5d1d
marcinlewinski <marcinlewinski71@gmail.com>
2023-11-23 19:19:36 +0100

    Simple accordion window has been implemented

--- wild-event-manager-application/src/pages/mappage/MapPage.jsx
+++ wild-event-manager-application/src/pages/mappage/MapPage.jsx
@@ -2,18 +2,32 @@ import React, { useEffect, useState } from "react";
 import Map from "../../components/map/Map"
 import EventsButton from "../../components/buttons/EventsButton";
 import { useMap } from "../../services/providers/MapProvider";
+import './MapStyle.scss';
 
 
 
 const MapPage = () => {
 	const { map: contextMap } = useMap();
+	const [isDivExpanded, setIsDivExpanded] = useState(false);
 
+	const handleToggleDivSize = () => {
+		setIsDivExpanded(!isDivExpanded);
+	};
 	return (
 		Object.keys(contextMap).length > 0 ?
-			<div>
+			<div >
 				<EventsButton />
 				<Map contextMap={contextMap} />
-			</div>
+				
+				<div class='box'>
+				<div class="center">
+					<section id="item1">
+						<a href="#item1">Lorem Ipum is the dummy text</a>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vel beatae consequatur suscipit vero magnam nulla molestias alias praesentium atque, commodi error quis, unde aliquam distinctio blanditiis! Beatae, temporibus reprehenderit!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vel beatae consequatur suscipit vero magnam nulla molestias alias praesentium atque, commodi error quis, unde aliquam distinctio blanditiis! Beatae, temporibus reprehenderit!</p>
+					</section> 
+				</div>
+				</div>
+			</div> 
 			: null
 
 
