1dc2f602da95d1d2cab039dac3c6821d4c80fbf6
marcinlewinski <marcinlewinski71@gmail.com>
2023-11-29 20:25:37 +0100

    Accordion expansion or collapse can be toggled

--- wild-event-manager-application/src/pages/mappage/MapPage.jsx
+++ wild-event-manager-application/src/pages/mappage/MapPage.jsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useState } from "react";
-import Map from "../../components/map/Map"
+import React, { useState } from "react";
+import Map from "../../components/map/Map";
 import EventsButton from "../../components/buttons/EventsButton";
 import { useMap } from "../../services/providers/MapProvider";
 import './MapStyle.scss';
-
-
 
 const MapPage = () => {
 	const { map: contextMap } = useMap();
@@ -13,26 +11,37 @@ const MapPage = () => {
 	const handleToggleDivSize = () => {
 		setIsDivExpanded(!isDivExpanded);
 	};
+
 	return (
-		Object.keys(contextMap).length > 0 ?
-			<div >
+		Object.keys(contextMap).length > 0 ? (
+			<div>
 				<EventsButton />
 				<Map contextMap={contextMap} />
-				
-				<div class='box'>
-				<div class="center">
-					<section id="item1">
-						<a href="#item1">Lorem Ipum is the dummy text</a>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vel beatae consequatur suscipit vero magnam nulla molestias alias praesentium atque, commodi error quis, unde aliquam distinctio blanditiis! Beatae, temporibus reprehenderit!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vel beatae consequatur suscipit vero magnam nulla molestias alias praesentium atque, commodi error quis, unde aliquam distinctio blanditiis! Beatae, temporibus reprehenderit!</p>
-					</section> 
+
+				<div className='box' onClick={handleToggleDivSize}>
+					<div className="center">
+						<section id="item1">
+							<div>
+									<div>
+										<a href="#item1">=</a>
+									</div>
+									{/* <div className={isDivExpanded ? "accordion__icon" : "accordion__icon_expanded"}>
+										⯆
+									</div> */}
+							
+								<div>
+									<p className={isDivExpanded ? 'expanded-content' : 'collapsed-content'}>
+										Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vel beatae consequatur suscipit vero magnam nulla molestias alias praesentium atque, commodi error quis, unde aliquam distinctio blanditiis! Beatae, temporibus reprehenderit!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vel beatae consequatur suscipit vero magnam nulla molestias alias praesentium atque, commodi error quis, unde aliquam distinctio blanditiis! Beatae, temporibus reprehenderit!
+									</p>
+								</div>
+
+							</div>
+						</section>
+					</div>
 				</div>
-				</div>
-			</div> 
-			: null
-
-
-
-	)
+			</div>
+		) : null
+	);
 }
 
-export default MapPage
+export default MapPage;
