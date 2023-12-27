bc7a20e652b118e807cf6e27806b2f5ef501f739
marcinlewinski <marcinlewinski71@gmail.com>
2023-12-27 19:56:09 +0100

    Added condition for the absence of events

--- wild-event-manager-application/src/pages/mappage/Slider.jsx
+++ wild-event-manager-application/src/pages/mappage/Slider.jsx
@@ -1,45 +1,32 @@
 import React from 'react';
 import { useEventToday } from '../../services/providers/EventTodayProvider';
 import { RemainingTime } from './RemainingTime';
-import { Link } from 'react-router-dom';
 import { useGlobalState } from '../../services/providers/GlobalGotoLocationProvider';
 
 
 
 const Slider = () => {
   const eventsToday = useEventToday();
-  // const closestEvent = findTheClosestEvent();
-  const { selectedLocation, setGlobalLocation } = useGlobalState();
 
+  const { setGlobalLocation } = useGlobalState();
 
-  if (!eventsToday || eventsToday.length === 0) {
-    return null;
-  }
-
-  // function findTheClosestEvent() {
-  //   if (eventsToday.length === 0) {
-  //     return null;
-  //   }
-
-  //   let closestEvent = eventsToday[0];
-
-  //   const currentTime = new Date();
-
-  //   eventsToday.forEach((event) => {
-  //     const eventTime = new Date(event.startsAt);
-
-  //     if (eventTime > currentTime && eventTime < new Date(closestEvent.startsAt)) {
-  //       closestEvent = event;
-  //     }
-  //   });
-
-  //   return closestEvent;
-  // }
 
   return (
     <div className='accordion_body'>
-      <div className="track-wrapper" key={Math.random(Math.floor() / 100)}>
+      <div className="track-wrapper">
         <ul className="track">
+         {eventsToday.length === 0 ? (
+          <li className="track__item">
+          <div className='content' >
+            <p>
+            There is no upcoming events!
+              </p>
+          </div>  
+          </li>
+          
+         ):(
+         null
+         )}
           {eventsToday.map(event => (
             <li className="track__item" key={event.id}>
               <div className='content' >
