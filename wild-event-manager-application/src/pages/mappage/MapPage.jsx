5e11192e9a931028fc6318033d5dd5e63d41cf0a
marcinlewinski <marcinlewinski71@gmail.com>
2023-12-27 20:36:47 +0100

    REFACTOR: Moving Accordion stuff into a separate folder

--- wild-event-manager-application/src/pages/mappage/MapPage.jsx
+++ wild-event-manager-application/src/pages/mappage/MapPage.jsx
@@ -3,7 +3,7 @@ import Map from "../../components/map/Map";
 import EventsButton from "../../components/buttons/EventsButton";
 import { useMap } from "../../services/providers/MapProvider";
 import './MapStyle.scss';
-import AccordionWithSlider from "./AccordionWithSlider"; 
+import AccordionWithSlider from "../../components/accordion/AccordionWithSlider"; 
 
 const MapPage = () => {
   const { map: contextMap } = useMap();
@@ -20,7 +20,6 @@ const MapPage = () => {
         <EventsButton />
         <Map contextMap={contextMap} />
         <AccordionWithSlider  isDivExpanded={isDivExpanded} handleToggleDivSize={handleToggleDivSize} />
-
       </div>
     ) : null
   );
